c7af157aa9d33f8811df1ccb9085270a64efb378
ektravel <38017980+ektravel@users.noreply.github.com>
2018-05-23 12:43:55 -0500

    adding code for landing page 'index.html'

---
+++ server.js
@@ -0,0 +1,15 @@
+// Create a basic server using Express.JS
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require ("path");
+
+var app = express();
+var PORT = 3000;
+// * Create a few array variables that will hold the data
+var 
+
+// * Create a set of routes for getting and posting table data
+
+// * Create a set of routes for displaying the HTML pages
+
+// * Use jQuery to run AJAX calls to GET and POST data from users to the Express server
